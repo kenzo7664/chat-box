10427d191548bf245de7605b492fcc93b5db04b6
kenzo7664 <olaotanemmanuel1@gmail.com>
2022-09-20 17:41:30 +0100

    structure of the chat box

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+
+const App = () => {
+	return (
+		<ChatEngine
+			height='100vh'
+			projectID='da1597e0-9aa8-49f8-b3b4-4569afbe1d5d'
+			userName='messi'
+			userSecret='Messi'
+		/>
+	);
+};
+
+export default App;
